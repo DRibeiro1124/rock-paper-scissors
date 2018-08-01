9b6d1d00d3844e49e6ddb0fb0420d0daae212348
Dino Ribeiro <DRibeiro1124@gmail.com>
2018-07-31 20:45:10 -0400

    initalize JS codes

--- public/main.js
+++ public/main.js
@@ -1,5 +1,31 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
+const playerOneRockButtonEvent = () => {
+  console.log('Player one rock button clicked')
+  playerOneSelects = 'rock'
+}
+const playerOnePaperButtonEvent = () => {
+  console.log('Player One paper button clicked')
+  playerOneSelects = 'paper'
+}
+const playerOneScissorsButtonEvent = () => {
+  console.log('Player One scissors button clicked')
+  playerOneSelects = 'scissors'
+}
+const playerTwoRockButtonEvent = () => {
+  console.log('Player Two rock button clicked')
+  playerTwoSelects = 'rock'
+}
+const playerTwoPaperButtonEvent = () => {
+  console.log('Player Two paper button clicked')
+  playerTwoSelects = 'paper'
+}
+const playerTwoScissorsButtonEvent = () => {
+  console.log('Player Two scissors button clicked')
+  playerTwoSelects = 'scissors'
 }
 
-document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.p1-rock-button').addEventListener('click', playerOneRockButtonEvent) 
+document.querySelector('.p1-paper-button').addEventListener('click', playerOnePaperButtonEvent)
+document.querySelector('.p1-scissors-button').addEventListener('click', playerOneScissorsButtonEvent)
+document.querySelector('.p2-rock-button').addEventListener('click', playerTwoRockButtonEvent)
+document.querySelector('.p2-paper-button').addEventListener('click', playerTwoPaperButtonEvent)
+document.querySelector('.p2-scissors-button').addEventListener('click', playerTwoScissorsButtonEvent)
